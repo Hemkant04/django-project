08cb09267a8dbef6478a255ded104207f9bf31b4
Hemkant04 <hemkanta04@gmail.com>
2026-02-17 16:54:09 +0545

    add gunicorn

--- core_project/settings.py
+++ core_project/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DEBUG = os.getenv() 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
